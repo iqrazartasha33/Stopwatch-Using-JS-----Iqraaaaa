ab06f9d4200cf9ce6f7d668999c07aa501511347
PirateCoderz <118294175+PirateCoderz@users.noreply.github.com>
2024-11-08 21:57:14 +0500

    A little changes in basic structure, in js file text change on start pause, in css file change color of text.

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ function startStop() {
   if (isRunning) {
     clearInterval(timer);
     isRunning = false;
+    document.getElementById('startbtn').innerText = "Start";
   } else {
     timer = setInterval(updateTime, 1000);
     isRunning = true;
+    document.getElementById('startbtn').innerText = "Pause";
   }
 }
 
